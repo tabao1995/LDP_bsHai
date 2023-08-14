451e336abe72715a9320511112c70bf9f0e51256
baota <baota@efe.com.vn>
2023-08-15 01:48:11 +0700

    update

---
+++ assets/js/mystyle.js
@@ -0,0 +1,19 @@
+    $(document).ready(function () {
+    $(".slick.khay").slick({
+        slidesPerRow: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        infinite: true,
+    });
+    $(".section11__chungnhan").slick({
+        slidesPerRow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        infinite: true,
+    });
+    $(".section13__review").slick({
+        slidesPerRow: 2,
+        slidesToScroll: 1,
+    });
+    });
